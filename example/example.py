f138480abe1e2676d1450b53712af2ec386b0127
MyApaulogies <MyApaulogies@tuta.io>
2023-04-18 01:20:34 -0600

    remove mention of `jsonpickle`

--- example/example.py
+++ example/example.py
@@ -1,11 +1,10 @@
-# requires: pip install jsonpickle
 
 import pokepastes_scraper as pastes
 import os
 
 team = pastes.team_from_url('https://pokepast.es/5c46f9ec443664cb')
-thisdir = os.path.dirname(__file__)
-with open(f'{thisdir}/example_team.json', 'w') as f:
+this_dir = os.path.dirname(__file__)
+with open(f'{this_dir}/example_team.json', 'w') as f:
     f.write(team.to_json())
 
 for mon in team.members:
